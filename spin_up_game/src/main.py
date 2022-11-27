b20103f0ddfcfd40524256ad9d638a014af5f57d
Nikhil Mukraj <nikhilmukraj@gmail.com>
2022-11-27 17:05:26 -0500

    Update main.py
    added turning

--- spin_up_game/src/main.py
+++ spin_up_game/src/main.py
@@ -49,19 +49,35 @@ def rotate(x, y):
 
     return x, y
 
-def parse(axis, velo):
-    if velo > 0:
-        axis.spin(FORWARD)
+def parse(axis, velo, override=None):
+    if override is not None:
+        axis.spin(override)
     else:
-        axis.spin(REVERSE)
+        if velo > 0:
+            axis.spin(FORWARD)
+        else:
+            axis.spin(REVERSE)
 
     axis.set_velocity(velo, PERCENT)
 
-def jacob_has_a_onehundred_incher():
+def drive_mapping():
     while True: # infinite looping or else this check can only run once
 
         x_pos = controller.axis4.position()
         y_pos = controller.axis3.position()
+
+        isRpressed = controller.buttonR1.pressing() 
+        isLpressed = controller.buttonL1.pressing()
+
+        # change turning passed on trigger buttons
+
+        motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, isLpressed)
+        motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, isRpressed)
+        motor_3 = Motor(Ports.PORT3, GearSetting.RATIO_18_1, not isLpressed)
+        motor_4 = Motor(Ports.PORT4, GearSetting.RATIO_18_1, not isRpressed)
+
+        drive_x = MotorGroup(motor_1, motor_3)
+        drive_y = MotorGroup(motor_2, motor_4)
 
         # convert to polar
         # rotate 45 degrees
@@ -78,4 +94,4 @@ def jacob_has_a_onehundred_incher():
         else:
             drive_y.stop()
 
-jacob_has_a_onehundred_incher()
+drive_mapping()
