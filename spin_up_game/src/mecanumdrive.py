ec12a4fe1a974bd8ccabf17a2c36e2c6ea7b992c
Nikhil Mukraj <nikhilmukraj@gmail.com>
2022-12-01 15:30:48 -0500

    Update mecanumdrive.py
    basic docs added, readme will be added later

--- spin_up_game/src/mecanumdrive.py
+++ spin_up_game/src/mecanumdrive.py
@@ -28,6 +28,13 @@ import math
 # Begin project code
 controller = Controller()
 
+# Ports
+
+# Front
+# 3 4
+# 2 1
+# Back
+
 motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
 motor_3 = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
@@ -36,38 +43,71 @@ motor_4 = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
 drive_x = MotorGroup(motor_1, motor_3)
 drive_y = MotorGroup(motor_2, motor_4)
 
-def rotate(x, y):
+def rotate(x, y, rotation_angle=math.pi / 4):
+    '''
+    Converts Cartesian coordinates into polar coordinates, 
+    and rotates coordinates around origin by `rotation_angle` radian,
+    converts back and returns the rotated Cartesian coordinates
+    '''
     deg = math.atan2(y, x)
     r = math.sqrt(y ** 2 + x ** 2)
 
     # deg in radians
 
-    deg += math.pi / 4
+    deg += rotation_angle
 
     x = r * math.cos(deg)
     y = r * math.sin(deg)
 
     return x, y
 
-def parse(axis, velo):
+def parse(axis, velo, units=RPM):
+    '''
+    Changes the axis direction based on the sign of velocity,
+    sets the axis velocity to the velocity and units (either `RPM` or `PERCENT`)
+    '''
     if velo > 0:
         axis.spin(FORWARD)
     else:
         axis.spin(REVERSE)
 
-    axis.set_velocity(velo, RPM)
+    axis.set_velocity(velo, units)
 
 def drive_mapping():
-    while True: # infinite looping or else this check can only run once
-
+    '''
+    Maps the drive to the left joystick omnidirectionally and turning based on triggers
+    '''
+    while True: 
+        # obtain position of joystick in x and y
         x_pos = controller.axis4.position()
         y_pos = controller.axis3.position()
+        
+        # change turning passed on trigger buttons
 
         isRpressed = controller.buttonR1.pressing() 
         isLpressed = controller.buttonL1.pressing()
-
-        # change turning passed on trigger buttons
-
+        
+        # without turning:
+        
+        # 3 counterclockwise (true) 4 counterclockwise (true) 
+        # 2 clockwise (false)       1 clockwise (false)
+        
+        # 3 and 4 are different since they are angled differently on the actual robot
+        
+        # with turning (left):
+        
+        # 3 clockwise (false) 4 counterclockwise (true) 
+        # 2 clockwise (false) 1 counterclockwise (true)
+        
+        # left side moves in one direction while right side moves in other direction, causing leftward turn
+        
+        # with turning (right):
+        
+        # 3 clockwise (true) 4 counterclockwise (false) 
+        # 2 clockwise (true) 1 counterclockwise (false)
+        
+        # same concept but opposite directions causing rightward turning
+    
         motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, isLpressed)
         motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, isRpressed)
         motor_3 = Motor(Ports.PORT3, GearSetting.RATIO_18_1, not isLpressed)
@@ -78,13 +118,12 @@ def drive_mapping():
         drive_x = MotorGroup(motor_1, motor_3)
         drive_y = MotorGroup(motor_2, motor_4)
 
-        # convert to polar
-        # rotate 45 degrees
-        # convert back
-
         x_pos, y_pos = rotate(x_pos, y_pos)
-        x_pos, y_pos = x_pos * 5, y_pos * 5 # converts to max of 500 rpm
+        x_pos, y_pos = x_pos * 5, y_pos * 5 # converts to max of 500 rpm (range is 0 to 500) 
+        
+        # max of 500 rpm, any more does not seem to make the robot faster but could be tuned further
 
+        # motors cannot seem to interpret a 0 velocity, have to manually stop motors at 0 velocity
         if x_pos != 0:
             parse(drive_x, x_pos)
         else:
